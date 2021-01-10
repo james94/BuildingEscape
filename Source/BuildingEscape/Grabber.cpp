b004cede29ae551eb7d11e8192d3a10ab681140d
james94 <jamesmedel94@gmail.com>
2021-01-09 18:56:50 -0600

    Reduced Code in Hot Loop TickComponent
    - Refactored alot of code: created SetupInputComponent(), FindPhysicsHandle() and GetFirstPhysicsBodyInReach()
    functions
    - So now instead of getting players viewpoint, ray casting out to a certain distance and seeing what it hits being
    done in TickComponent(), it is done when user presses right mouse click or in other words, performs a grab action.

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -13,17 +13,32 @@ UGrabber::UGrabber()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
-
 
 // Called when the game starts
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	// Checking for Physics Handle Component
+	FindPhysicsHandle();
+	SetupInputComponent();
+
+}
+
+void UGrabber::SetupInputComponent()
+{
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+	if(InputComponent)
+	{
+		// UE_LOG(LogTemp, Warning, TEXT("Found Input Component on %s!"), *(GetOwner()->GetName()));
+		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+		InputComponent->BindAction("Release", IE_Released, this, &UGrabber::Release);
+	}
+}
+
+// Checking for Physics Handle Component
+void UGrabber::FindPhysicsHandle()
+{
 	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
 	if(PhysicsHandle)
 	{
@@ -33,28 +48,24 @@ void UGrabber::BeginPlay()
 	{
 		UE_LOG(LogTemp, Error, TEXT("No Physics Handle Component on %s!"), *(GetOwner()->GetName()));
 	}
-	
-	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
-	if(InputComponent)
-	{
-		// UE_LOG(LogTemp, Warning, TEXT("Found Input Component on %s!"), *(GetOwner()->GetName()));
-		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
-		InputComponent->BindAction("Release", IE_Released, this, &UGrabber::Release);
-	}
-	// else
-	// {
-	// 	UE_LOG(LogTemp, Error, TEXT("Input Component MISSING on %s!"), *(GetOwner()->GetName()));
-	// }
 }
 
 void UGrabber::Grab()
 {
 	UE_LOG(LogTemp, Warning, TEXT("Grabber Pressed"));
+
+	// Only raycast when key is pressed and see if we reach any actors with physics body collision channel set
+	GetFirstPhysicsBodyInReach();
+
+	// If we hit something then attach the physics handle.
+
+	// TODO: attach physics handle
 }
 
 void UGrabber::Release()
 {
 	UE_LOG(LogTemp, Warning, TEXT("Grabber Released"));
+	// TODO: remove/release the physics handle
 }
 
 // Called every frame
@@ -62,6 +73,12 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	// If the physics handle is attached
+		// Move the object we are holding
+}
+
+FHitResult UGrabber::GetFirstPhysicsBodyInReach() const
+{
 	// Get players viewpoint
 	FVector PlayerViewPointLocation;
 	FRotator PlayerViewPointRotation;
@@ -72,17 +89,6 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 
 	// Draw a line from player showing the reach. LineTraceEnd = Vector + UnitVector * Reach
 	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
-
-	DrawDebugLine(
-		GetWorld(),
-		PlayerViewPointLocation,
-		LineTraceEnd,
-		FColor(0, 255, 0),
-		false,
-		0.f,
-		0,
-		5.f
-	);
 
 	FHitResult Hit;
 	// Ray-cast out to a certain distance (Reach)
@@ -103,7 +109,7 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	{
 		UE_LOG(LogTemp, Warning, TEXT("Line Trace has hit: %s"), *(ActorHit->GetName()));
 	}
-	// Logging out to test
-
+	
+	return Hit;
 }
 
--- Source/BuildingEscape/Grabber.h
+++ Source/BuildingEscape/Grabber.h
@@ -17,13 +17,12 @@ public:
 	// Sets default values for this component's properties
 	UGrabber();
 
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 private:
 	float Reach = 200.f; // 1 meter reach
@@ -33,4 +32,10 @@ private:
 
 	void Grab();
 	void Release();
+	void FindPhysicsHandle();
+	void SetupInputComponent();
+
+	// Return the first Actor within reach with a physics body.
+	// const since we don't want it changing what it ends up getting hit by
+	FHitResult GetFirstPhysicsBodyInReach() const;
 };
